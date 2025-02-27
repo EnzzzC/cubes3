d2b6989025ad7e57941c69d856a53e7867cc7017
COLONNA Enzo <enzo.colonna@montpellier.fr>
2025-02-27 08:34:30 +0100

    test pipeline

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,14 +3,6 @@ const app = require('../index');
 
 let server;
 
-beforeAll(() => {
-  server = app.listen(); // Démarrer le serveur si nécessaire
-});
-
-afterAll(done => {
-  server.close(done); // Fermer proprement le serveur après les tests
-});
-
 describe('GET /', () => {
   it('devrait retourner un message de bienvenue', async () => {
     const res = await request(app).get('/');
